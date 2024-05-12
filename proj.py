d17659d541c49b278648120f5d7c5d06a7927763
shivajimanthri <manthrishivaji13579@gmail.com>
2024-05-12 12:27:38 +0530

    subbranch for main

--- proj.py
+++
@@ -1,9 +0,0 @@
-'''
-Enumerating objects: 14, done.
-Counting objects: 100% (14/14), done.
-Delta compression using up to 8 threads
-Compressing objects: 100% (12/12), done.
-Writing objects: 100% (14/14), 1.18 KiB | 121.00 KiB/s, done.
-Total 14 (delta 1), reused 0 (delta 0), pack-reused 0
-remote: Resolving deltas: 100% (1/1), done.
-'''
